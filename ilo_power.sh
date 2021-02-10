f33e944451f44c64c61634e617644fc1bc85c1a0
Cliffton <feralcoder@gmail.com>
2021-02-09 18:00:18 -0800

    Add wait_for_* to power_on/off commands.

--- ilo_power.sh
+++ ilo_power.sh
@@ -9,34 +9,90 @@ ilo_power_get_state () {
   echo "$HOST is $STATE"
 }
 
-ilo_power_off () {
-  local HOST=$1 IP=$2 COUNT
-  local STATE=""
-  while [[ $STATE != "Off" ]]; do
-    OUTPUT=$(ssh -i ~/.ssh/id_rsa_ilo2 $IP -l stack "power off")
-    for COUNT in `seq 1 3`; do
-      sleep 3
-      STATE=$(ilo_power_get_state $HOST $IP | awk '{print $3}')
-      [[ $STATE == "Off" ]] && break
-      echo "$HOST still powered on, checking again..."
-    done
+ilo_power_wait_for_off () {
+  local HOST=$1 IP=$2
+  local COUNT INTERVAL
+  [[ "$3" != "" ]] && COUNT=$3 || COUNT=5
+  [[ "$4" != "" ]] && INTERVAL=$4 || INTERVAL=10
+
+  local ITER STATE
+  for ITER in `seq 1 $COUNT`; do
+    echo "Checking power state of $HOST..."
+    STATE=$(ilo_power_get_state $HOST $IP | awk '{print $3}')
+    [[ $STATE == "Off" ]] && return 0
+    [[ "$ITER" != "$COUNT" ]] || { echo "$HOST did not power off!"; return 1; }
+    echo "$HOST still powered on, waiting $INTERVAL seconds to check again..."
+    sleep $INTERVAL
   done
-  echo "$HOST is powered off."
+  # We should have returned 0 or 1 already, from within loop...
+}
+
+ilo_power_wait_for_on () {
+  local HOST=$1 IP=$2
+  local COUNT INTERVAL
+  [[ "$3" != "" ]] && COUNT=$3 || COUNT=5
+  [[ "$4" != "" ]] && INTERVAL=$4 || INTERVAL=10
+
+  local ITER STATE
+  for ITER in `seq 1 $COUNT`; do
+    echo "Checking power state of $HOST..."
+    STATE=$(ilo_power_get_state $HOST $IP | awk '{print $3}')
+    [[ $STATE == "On" ]] && return 0
+    [[ "$ITER" != "$COUNT" ]] || { echo "$HOST did not power on!"; return 1; }
+    echo "$HOST still powered off, waiting $INTERVAL seconds to check again..."
+    sleep $INTERVAL
+  done
+  # We should have returned 0 or 1 already, from within loop...
+}
+
+ilo_power_off () {
+  local HOST=$1 IP=$2
+  local COUNT INTERVAL
+  [[ "$3" != "" ]] && COUNT=$3 || COUNT=5
+  [[ "$4" != "" ]] && INTERVAL=$4 || INTERVAL=10
+
+  echo "Powering off $HOST (soft)..."
+  local OUTPUT=$(ssh -i ~/.ssh/id_rsa_ilo2 $IP -l stack "power off")
+
+  if ( ilo_power_wait_for_off $HOST $IP $COUNT $INTERVAL ) ; then
+    echo "$HOST is powered off."
+  else
+    echo "$HOST did not power off.  Time to hard reset!"
+    OUTPUT=$(ssh -i ~/.ssh/id_rsa_ilo2 $IP -l stack "power off hard")
+    if ( ilo_power_wait_for_off $HOST $IP 2 5 ) ; then
+      echo "$HOST is powered off hard."
+    else
+      echo "$HOST did not hard power off.  Time to use ipmi chassis control!"
+      OUTPUT=$(ipmitool -I lanplus -H $IP -U stack -f ilo_pass chassis power off)
+      if [[ $(echo "$OUTPUT" | grep 'Down.Off') == "" ]]; then
+        echo "FAILED to power off $HOST!"
+        return 1
+      else
+        echo "$HOST is powered off via chassis control."
+      fi
+    fi
+  fi
 }
 
 ilo_power_on () {
   local HOST=$1 IP=$2 COUNT
-  local STATE=""
-  while [[ $STATE != "On" ]]; do
-    OUTPUT=$(ssh -i ~/.ssh/id_rsa_ilo2 $IP -l stack "power on")
-    for COUNT in `seq 1 3`; do
-      sleep 3
-      STATE=$(ilo_power_get_state $HOST $IP | awk '{print $3}')
-      [[ $STATE == "On" ]] && break
-      echo "$HOST still powered off, checking again..."
-    done
-  done
-  echo "$HOST is powered on."
+  local COUNT INTERVAL
+  [[ "$3" != "" ]] && COUNT=$3 || COUNT=5
+  [[ "$4" != "" ]] && INTERVAL=$4 || INTERVAL=10
+
+  local OUTPUT=$(ssh -i ~/.ssh/id_rsa_ilo2 $IP -l stack "power on")
+  if ( ilo_power_wait_for_on $HOST $IP $COUNT $INTERVAL ) ; then
+    echo "$HOST is powered on".
+  else
+    echo "$HOST did not power on.  Turning on via ipmi chassis power on."
+    OUTPUT=$(ipmitool -I lanplus -H $IP -U stack -f ilo_pass chassis power on)
+    if [[ $(echo "$OUTPUT" | grep 'Up.On') == "" ]]; then
+      echo "FAILED to power on $HOST!"
+      return 1
+    else
+      echo "$HOST is powered on via chassis control."
+    fi
+  fi
 }
 
 ilo_power_off_these_hosts () {
