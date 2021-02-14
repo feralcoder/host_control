da3cfdc3927dff27fdf162b0ead02d5f18774850
feralcoder <feralcoder@gmail.com>
2021-02-14 23:59:11 +0000

    Move host groups to control_scripts.sh.

--- control_scripts.sh
+++ control_scripts.sh
@@ -12,3 +12,19 @@ CONTROL_SOURCE="${BASH_SOURCE[0]}"
 . os_control.sh  
 . undercloud_control.sh
 . stack_control.sh  
+
+ILO2_HOSTS="mrl gnd yda dmb"
+ILO4_HOSTS="mtn lmn bmn neo str kgn"
+PRIMARY_CONTROL_HOSTS="str"
+SECONDARY_CONTROL_HOSTS="mrl"
+TERNARY_CONTROL_HOSTS="gnd"
+CONTROL_HOSTS="$PRIMARY_CONTROL_HOSTS $SECONDARY_CONTROL_HOSTS $TERNARY_CONTROL_HOSTS"
+COMPUTE_HOSTS="mtn lmn bmn neo"
+
+ALL_HOSTS="mtn lmn bmn kgn neo str mrl gnd yda dmb"
+UNDERCLOUD_HOSTS="kgn dmb"
+OVERCLOUD_HOSTS="mtn lmn bmn neo mrl gnd str"
+
+ALL_HOSTS_MINUS_ADMIN="mtn lmn bmn kgn neo str mrl gnd dmb"
+
+ALL_HOSTS_LONGNAMES="manhattan lawnmowerman bowman kerrigan neo strange merlin gandalf yoda dumbledore"
--- ilo_common.sh
+++ ilo_common.sh
@@ -6,20 +6,3 @@ DEV_HD=3
 DEV_USB=4
 DEV_PXE=5
 
-ILO2_HOSTS="mrl gnd yda dmb"
-ILO4_HOSTS="mtn lmn bmn neo str kgn"
-PRIMARY_CONTROL_HOSTS="str"
-SECONDARY_CONTROL_HOSTS="mrl"
-TERNARY_CONTROL_HOSTS="gnd"
-CONTROL_HOSTS="$PRIMARY_CONTROL_HOSTS $SECONDARY_CONTROL_HOSTS $TERNARY_CONTROL_HOSTS"
-COMPUTE_HOSTS="mtn lmn bmn neo"
-
-ALL_HOSTS="mtn lmn bmn kgn neo str mrl gnd yda dmb"
-UNDERCLOUD_HOSTS="kgn dmb"
-OVERCLOUD_HOSTS="mtn lmn bmn neo mrl gnd str"
-
-ALL_HOSTS_MINUS_ADMIN="mtn lmn bmn kgn neo str mrl gnd dmb"
-
-
-
-ALL_HOSTS_LONGNAMES="manhattan lawnmowerman bowman kerrigan neo strange merlin gandalf yoda dumbledore"
