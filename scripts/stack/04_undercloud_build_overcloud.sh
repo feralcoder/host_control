0e331178e07d5e81b59be3c2a803747f02d861ab
feralcoder <feralcoder@gmail.com>
2021-02-15 08:06:08 +0000

    We're doing this!

---
+++ scripts/stack/04_undercloud_build_overcloud.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+THIS_SOURCE="$(dirname ${BASH_SOURCE[0]})"
+echo Running scripts from: $THIS_SOURCE
+
+. $THIS_SOURCE/../../control_scripts.sh
+
+os_control_boot_to_target_installation default dmb
+ssh_control_run_as_user cliff "cd ~cliff/CODE/feralcoder/train8 ; git pull" dmb
+
+ssh_control_run_as_user root "rm /root/overcloud_install*; tmux new-session -d -s overcloud 'DEFOPTS=true /home/cliff/CODE/feralcoder/train8/setup_overcloud.sh > overcloud_install_\$\$.log'; tmux split-window -t overcloud -h 'tail -f /root/overcloud_install_\$\$'" dmb
+
+
+
