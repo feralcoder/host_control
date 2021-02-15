e3546eacb7aaa6d2a78a7b9f0d74b9e84ef107ca
feralcoder <feralcoder@gmail.com>
2021-02-15 19:40:19 +0000

    04_undercloud_build_overcloud.sh

--- scripts/stack/04_undercloud_build_overcloud.sh
+++ scripts/stack/04_undercloud_build_overcloud.sh
@@ -7,7 +7,7 @@ echo Running scripts from: $THIS_SOURCE
 os_control_boot_to_target_installation default dmb
 ssh_control_run_as_user cliff "cd ~cliff/CODE/feralcoder/train8 ; git pull" dmb
 
-ssh_control_run_as_user root "rm /root/overcloud_install*; tmux new-session -d -s overcloud 'DEFOPTS=true /home/cliff/CODE/feralcoder/train8/setup_overcloud.sh > overcloud_install_\$\$.log'; tmux split-window -t overcloud -h 'tail -f /root/overcloud_install_\$\$'" dmb
+ssh_control_run_as_user root "rm /root/overcloud_install*; tmux new-session -d -s overcloud 'DEFOPTS=true /home/cliff/CODE/feralcoder/train8/setup_overcloud.sh > overcloud_install_\$\$.log'; sleep 5; tmux split-window -t overcloud -h 'tail -f /root/overcloud_install_*'" dmb
 
 
 
