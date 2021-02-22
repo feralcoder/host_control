15dcf858e43e1a4b2268f42de68230cfd5fa997b
feralcoder <feralcoder@gmail.com>
2021-02-22 07:36:57 +0000

    fix_and_freshen_repos update

--- scripts/fix_and_freshen_repos_everywhere.sh
+++ scripts/fix_and_freshen_repos_everywhere.sh
@@ -7,7 +7,7 @@ echo Running scripts from: $THIS_SOURCE
 ssh_control_run_as_user_these_hosts root "chown -R cliff:cliff /home/cliff/CODE" "$ALL_HOSTS"
 ssh_control_sync_as_user_these_hosts cliff /home/cliff/.git_password /home/cliff/.git_password "$ALL_HOSTS"
 
-for REPO in bootstrap-scripts workstation host_control train8; do
+for REPO in bootstrap-scripts  host_control  kolla-ansible  workstation  workstation.wiki; do
   CODE_PATH=/home/cliff/CODE/feralcoder/$REPO
   FIXPASS_CMD='sed -i "s/feralcoder:[^@]*/feralcoder:`cat ~/.git_password`/g" .git/config'
   ssh_control_run_as_user_these_hosts cliff "echo Fixing $REPO on \`hostname\`; if [[ -d $CODE_PATH ]]; then cd $CODE_PATH && $FIXPASS_CMD && git pull 2>&1; else echo $CODE_PATH DOES NOT EXIST ON \`hostname\`; fi; echo; echo" "$ALL_HOSTS"
