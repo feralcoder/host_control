1076613efe8709b2112c78d9d7c4567985180e63
feralcoder <feralcoder@gmail.com>
2021-02-22 12:17:12 +0000

    ordered for fastest running of ssh fetches...

--- scripts/fetch_host_keys_everywhere.sh
+++ scripts/fetch_host_keys_everywhere.sh
@@ -4,8 +4,13 @@ echo Running scripts from: $THIS_SOURCE
 
 . $THIS_SOURCE/../control_scripts.sh
 
+
 for host in $ALL_HOSTS; do
-  ssh_control_run_as_user cliff "cd ~cliff/CODE/feralcoder/host_control; . control_scripts.sh ; ilo_control_refetch_ilo_hostkey_these_hosts \"$ALL_HOSTS\"; ssh_control_refetch_hostkey_these_hosts  \"$ALL_HOSTS\"" $host
-  ssh_control_run_as_user root "cd ~cliff/CODE/feralcoder/host_control; . control_scripts.sh ; ilo_control_refetch_ilo_hostkey_these_hosts \"$ALL_HOSTS\"; ssh_control_refetch_hostkey_these_hosts  \"$ALL_HOSTS\"" $host
+  ssh_control_run_as_user cliff "cd ~cliff/CODE/feralcoder/host_control; . control_scripts.sh ; ssh_control_refetch_hostkey_these_hosts  \"$ALL_HOSTS\"" $host
+  ssh_control_run_as_user root "cd ~cliff/CODE/feralcoder/host_control; . control_scripts.sh ; ssh_control_refetch_hostkey_these_hosts  \"$ALL_HOSTS\"" $host
 done
 
+for host in $ALL_HOSTS; do
+  ssh_control_run_as_user cliff "cd ~cliff/CODE/feralcoder/host_control; . control_scripts.sh ; ilo_control_refetch_ilo_hostkey_these_hosts \"$ALL_HOSTS\"" $host
+  ssh_control_run_as_user root "cd ~cliff/CODE/feralcoder/host_control; . control_scripts.sh ; ilo_control_refetch_ilo_hostkey_these_hosts \"$ALL_HOSTS\"" $host
+done
