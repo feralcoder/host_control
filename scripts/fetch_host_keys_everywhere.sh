48851384837e10997602e856e999633008f7212a
feralcoder <feralcoder@gmail.com>
2021-02-22 11:57:04 +0000

    Get host keys for root also.

--- scripts/fetch_host_keys_everywhere.sh
+++ scripts/fetch_host_keys_everywhere.sh
@@ -4,5 +4,8 @@ echo Running scripts from: $THIS_SOURCE
 
 . $THIS_SOURCE/../control_scripts.sh
 
-ssh_control_run_as_user_these_hosts cliff "cd CODE/feralcoder/host_control; . control_scripts.sh ; for host in $ALL_HOSTS; do ilo_control_refetch_ilo_hostkey \"$ALL_HOSTS\"" $host; ssh_control_refetch_hostkey  \"$ALL_HOSTS\"" $host; done"
-ssh_control_run_as_user_these_hosts root "cd CODE/feralcoder/host_control; . control_scripts.sh ; for host in $ALL_HOSTS; do ilo_control_refetch_ilo_hostkey \"$ALL_HOSTS\"" $host; ssh_control_refetch_hostkey  \"$ALL_HOSTS\"" $host; done"
+for host in $ALL_HOSTS; do
+  ssh_control_run_as_user cliff "cd ~cliff/CODE/feralcoder/host_control; . control_scripts.sh ; ilo_control_refetch_ilo_hostkey_these_hosts \"$ALL_HOSTS\"; ssh_control_refetch_hostkey_these_hosts  \"$ALL_HOSTS\"" $host
+  ssh_control_run_as_user root "cd ~cliff/CODE/feralcoder/host_control; . control_scripts.sh ; ilo_control_refetch_ilo_hostkey_these_hosts \"$ALL_HOSTS\"; ssh_control_refetch_hostkey_these_hosts  \"$ALL_HOSTS\"" $host
+done
+
