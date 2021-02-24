f15849b0e2cc4bed610ebb4168017a797ce8cf08
feralcoder <feralcoder@gmail.com>
2021-02-24 07:10:19 +0000

    New: git_control_stash_drop, git_control_stash_explore, git_control_stash_changes

--- git_control.sh
+++ git_control.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 
 REPOS="bootstrap-scripts  host_control  kolla-ansible  workstation  workstation.wiki"
+CODE_PATH=/home/cliff/CODE/feralcoder/
+
 
 git_control_set_local_password () {
   echo "Setting new local git password..."
@@ -14,11 +16,12 @@ git_control_fix_repos () {
   [[ -f ~/.git_password ]] || git_control_set_local_password
   ssh_control_sync_as_user cliff ~/.git_password ~/.git_password $HOST
 
-  local REPO
+  local REPO REPO_PATH
   for REPO in $REPOS; do
-    CODE_PATH=/home/cliff/CODE/feralcoder/$REPO
+    REPO_PATH=$CODE_PATH/$REPO
     FIXPASS_CMD='sed -i "s/feralcoder:[^@]*/feralcoder:`cat ~/.git_password`/g" .git/config'
-    ssh_control_run_as_user cliff "echo Fixing $REPO on \`hostname\`; if [[ -d $CODE_PATH ]]; then cd $CODE_PATH && $FIXPASS_CMD 2>&1; else echo $CODE_PATH DOES NOT EXIST ON \`hostname\`; fi; echo; echo" $HOST
+    ssh_control_run_as_user cliff "echo Fixing $REPO on \`hostname\`; if [[ -d $REPO_PATH ]]; then cd $REPO_PATH && $FIXPASS_CMD 2>&1; else echo $REPO_PATH DOES NOT EXIST ON \`hostname\`; fi; echo; echo" $HOST
+    echo; echo
   done
 }
 
@@ -48,17 +51,18 @@ git_control_fix_repos_these_hosts () {
 }
 
 git_control_pull_push () {
-  HOST=$1
+  local HOST=$1
 
-  local REPO
+  local REPO REPO_PATH
   for REPO in $REPOS; do
-    CODE_PATH=/home/cliff/CODE/feralcoder/$REPO
-    ssh_control_run_as_user_these_hosts cliff "echo Fetch-Pushing $REPO on $HOST; if [[ -d $CODE_PATH ]]; then cd $CODE_PATH && git stash && git pull && git stash apply && git push 2>&1; else echo $CODE_PATH DOES NOT EXIST ON \`hostname\`; fi; echo; echo" $HOST
+    REPO_PATH=$CODE_PATH/$REPO
+    ssh_control_run_as_user_these_hosts cliff "echo Fetch-Pushing $REPO on $HOST; if [[ -d $REPO_PATH ]]; then cd $REPO_PATH && git pull 2>&1 ; PULL_ERROR=\$? ; if [[ \$PULL_ERROR == 0 ]]; then git push; else echo 'COULD NOT PULL $REPO on $HOST, Changes to stash?' ; fi; 2>&1; else echo $REPO_PATH DOES NOT EXIST ON \`hostname\`; fi; echo; echo" $HOST
+    echo; echo
   done
 }
 
 git_control_pull_push_these_hosts () {
-  HOST=$1
+  local HOST=$1
 
   local RETURN_CODE
   for HOST in $HOSTS now_wait; do
@@ -77,5 +81,33 @@ git_control_pull_push_these_hosts () {
       PIDS="$PIDS:$!"
       echo "Fetch-Push-ing repos on $HOST..."
     fi
+  done
+}
+
+git_control_stash_explore () {
+  local HOST=$1
+  local REPO REPO_PATH
+  for REPO in $REPOS; do
+    REPO_PATH=$CODE_PATH/$REPO
+    ssh_control_run_as_user_these_hosts cliff "echo Exploring Git Stashes in $REPO on $HOST; if [[ -d $REPO_PATH ]]; then cd $REPO_PATH && git stash list && git stash show -p && echo 'VS Working Copy:' && git diff stash@{0} 2>&1; else echo $REPO_PATH DOES NOT EXIST ON \`hostname\`; fi; echo; echo" $HOST
+    echo; echo
+  done
+}
+
+git_control_stash_changes () {
+  local STASH_REPOS=$1 HOST=$2
+  for REPO in $STASH_REPOS; do
+    REPO_PATH=$CODE_PATH/$REPO
+    ssh_control_run_as_user_these_hosts cliff "echo Stashing Changes in Git Repo $REPO on $HOST; if [[ -d $REPO_PATH ]]; then cd $REPO_PATH && git stash 2>&1; else echo $REPO_PATH DOES NOT EXIST ON \`hostname\`; fi; echo; echo" $HOST
+    echo; echo
+  done
+}
+
+git_control_stash_drop () {
+  local CLEAN_REPOS=$1 HOST=$2
+  for REPO in $CLEAN_REPOS; do
+    REPO_PATH=$CODE_PATH/$REPO
+    ssh_control_run_as_user_these_hosts cliff "echo Cleaning Git Stashes in $REPO on $HOST; if [[ -d $REPO_PATH ]]; then cd $REPO_PATH && git stash drop 2>&1; else echo $REPO_PATH DOES NOT EXIST ON \`hostname\`; fi; echo; echo" $HOST
+    echo; echo
   done
 }
