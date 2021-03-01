f69380163cf0d942f60e20df32c110eb2869ac8a
feralcoder <feralcoder@gmail.com>
2021-03-01 05:36:11 +0000

    Don't default to ALL_HOST for ad-hoc fix script.

--- macros/fix_all_ffs.sh
+++ macros/fix_all_ffs.sh
@@ -5,16 +5,16 @@ MACRO_DIR=$( dirname $MACRO_SOURCE )
 . ~/CODE/feralcoder/host_control/control_scripts.sh
 
 
-for HOST in $ALL_HOSTS; do
+for HOST in $THESE_HOSTS; do
   admin_control_bootstrap_admin $HOST
 done
 
-git_control_pull_push_these_hosts "$ALL_HOSTS"
-ssh_control_run_as_user_these_hosts cliff "~/CODE/feralcoder/workstation/update.sh" "$ALL_HOSTS"
-ssh_control_run_as_user_these_hosts root "rm /etc/ssh/ssh_host_\*/ -rf" "$ALL_HOSTS"
-admin_control_fix_grub_these_hosts "$ALL_HOSTS"
+git_control_pull_push_these_hosts "$THESE_HOSTS"
+ssh_control_run_as_user_these_hosts cliff "~/CODE/feralcoder/workstation/update.sh" "$THESE_HOSTS"
+ssh_control_run_as_user_these_hosts root "rm /etc/ssh/ssh_host_\*/ -rf" "$THESE_HOSTS"
+admin_control_fix_grub_these_hosts "$THESE_HOSTS"
 
-for HOST in $ALL_HOSTS; do
+for HOST in $THESE_HOSTS; do
   echo; echo "REFETCHING HOST KEYS on $HOST"
   ssh_control_run_as_user cliff "ssh_control_refetch_hostkey_these_hosts \"$ALL_HOSTS\"" $HOST 2>/dev/null
   echo; echo "Getting ILO hostkeys on $HOST"
