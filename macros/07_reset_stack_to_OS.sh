82f8b36715e01bc2ca3291151cde47874c08755b
feralcoder <feralcoder@gmail.com>
2021-03-02 20:00:28 +0000

    we should finish backups / restores in default OS

--- macros/07_reset_stack_to_OS.sh
+++ macros/07_reset_stack_to_OS.sh
@@ -5,3 +5,13 @@ MACRO_DIR=$( dirname $MACRO_SOURCE )
 . ~/CODE/feralcoder/host_control/control_scripts.sh
 
 $MACRO_DIR/09_reset_everything.sh 01_CentOS_8_3_Admin_Install a "$STACK_HOSTS"
+
+
+
+echo; echo "BOOTING TO DEFAULT OS ON $HOSTS"
+os_control_boot_to_target_installation_these_hosts default "$HOSTS"
+os_control_assert_hosts_booted_target default "$HOSTS" || {
+  echo "Not all hosts booted to default OS, check the environment!"
+  exit 1
+}
+echo; echo "ALL HOSTS ARE BOOTED TO default"
