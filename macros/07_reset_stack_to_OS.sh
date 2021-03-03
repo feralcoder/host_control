ac537cb8ac7748f88d6d3118e279f146cc8e084d
feralcoder <feralcoder@gmail.com>
2021-03-03 06:19:53 +0000

    and then shut down

--- macros/07_reset_stack_to_OS.sh
+++ macros/07_reset_stack_to_OS.sh
@@ -8,10 +8,12 @@ $MACRO_DIR/09_reset_everything.sh 01_CentOS_8_3_Admin_Install a "$STACK_HOSTS"
 
 
 
-echo; echo "BOOTING TO DEFAULT OS ON $HOSTS"
-os_control_boot_to_target_installation_these_hosts default "$HOSTS"
-os_control_assert_hosts_booted_target default "$HOSTS" || {
+echo; echo "BOOTING TO DEFAULT OS ON $STACK_HOSTS"
+os_control_boot_to_target_installation_these_hosts default "$STACK_HOSTS"
+os_control_assert_hosts_booted_target default "$STACK_HOSTS" || {
   echo "Not all hosts booted to default OS, check the environment!"
   exit 1
 }
 echo; echo "ALL HOSTS ARE BOOTED TO default"
+
+ilo_power_off_these_hosts "$STACK_HOSTS"
