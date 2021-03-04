1e92ec849d6d761db9a59f6150ee93cf1a6b278f
feralcoder <feralcoder@gmail.com>
2021-03-04 00:13:13 +0000

    run workstation/updates.sh

--- macros/02a_hosts_update.sh
+++ macros/02a_hosts_update.sh
@@ -43,6 +43,7 @@ host_control_updates () {
 #  for HOST in $HOSTS; do
 #    admin_control_bootstrap_admin $HOST
 #  done
+  ssh_control_run_as_user_these_hosts cliff "~/CODE/feralcoder/workstation/update.sh" "$HOSTS"
 
   echo; echo "UPDATING GIT REPOS EVERYWHERE"
   git_control_pull_push_these_hosts "$HOSTS" 2>/dev/null
