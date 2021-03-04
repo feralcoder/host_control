004f54b526e0fbf648855b523012bb37ee14238a
feralcoder <feralcoder@gmail.com>
2021-03-04 19:29:25 +0000

    Use HOSTS, not ALL_HOSTS.  Bail only if SELF is in backup list.

--- macros/09_backup_everything.sh
+++ macros/09_backup_everything.sh
@@ -14,14 +14,14 @@ BACKUPLINK=$1 DRIVESET=$2 HOSTS=$3
 # Run from admin box (yoda)
 
 # root users also need to know all hosts, for backup rsyncs
-#for HOST in $ALL_HOSTS; do
-#  ssh_control_run_as_user root ". ~cliff/CODE/feralcoder/host_control/control_scripts.sh; ssh_control_refetch_hostkey_these_hosts \"$ALL_HOSTS\"" $HOST
+#for HOST in $HOSTS; do
+#  ssh_control_run_as_user root ". ~cliff/CODE/feralcoder/host_control/control_scripts.sh; ssh_control_refetch_hostkey_these_hosts \"$HOSTS\"" $HOST
 #done
 #
-#ssh_control_run_as_user_these_hosts root "[[ -f ~/.ssh/id_rsa ]] || ssh-keygen -t rsa -P \"\" -f ~/.ssh/id_rsa" "$ALL_HOSTS"
-#ssh_control_run_as_user_these_hosts root "cat ~/.ssh/id_rsa.pub" "$ALL_HOSTS" | grep '^ssh' > /tmp/root_pubs
-#ssh_control_sync_as_user_these_hosts root /tmp/root_pubs /tmp/root_pubs "$ALL_HOSTS"
-#ssh_control_run_as_user_these_hosts root "cat ~/.ssh/authorized_keys /tmp/root_pubs | sort | uniq > /tmp/root_pubs_$$ ; cat /tmp/root_pubs_$$ > ~/.ssh/authorized_keys" "$ALL_HOSTS"
+#ssh_control_run_as_user_these_hosts root "[[ -f ~/.ssh/id_rsa ]] || ssh-keygen -t rsa -P \"\" -f ~/.ssh/id_rsa" "$HOSTS"
+#ssh_control_run_as_user_these_hosts root "cat ~/.ssh/id_rsa.pub" "$HOSTS" | grep '^ssh' > /tmp/root_pubs
+#ssh_control_sync_as_user_these_hosts root /tmp/root_pubs /tmp/root_pubs "$HOSTS"
+#ssh_control_run_as_user_these_hosts root "cat ~/.ssh/authorized_keys /tmp/root_pubs | sort | uniq > /tmp/root_pubs_$$ ; cat /tmp/root_pubs_$$ > ~/.ssh/authorized_keys" "$HOSTS"
 
 if [[ $DRIVESET == x ]]; then
   OPERATING_DRIVE=default
@@ -35,14 +35,16 @@ fi
 SELFNAME_SHORT=`hostname | awk -F'.' '{print $1}'`
 NAME_SUFFIX=`echo $SELFNAME_SHORT | awk -F'-' '{print $2}'`
 
+REBOOT_HOSTS=`group_logic_remove_self "$HOSTS"`
+
 [[ $NAME_SUFFIX == "" ]] && NAME_SUFFIX=default
-[[ $NAME_SUFFIX == "$OPERATING_DRIVE" ]] || {
+# if SELF NOT OPERATING DRIVE and SELF IN DO-LIST then BAIL
+( [[ $NAME_SUFFIX != "$OPERATING_DRIVE" ]] && [[ $REBOOT_HOSTS != $HOSTS ]] ) && {
   echo "RUN THIS SCRIPT FROM THE $OPERATING_DRIVE OS!"
   echo "This host will back itself up without reboots."
   exit 1
 }
 
-REBOOT_HOSTS=`group_logic_remove_self "$ALL_HOSTS"`
 os_control_boot_to_target_installation_these_hosts $OPERATING_DRIVE "$REBOOT_HOSTS"
 os_control_assert_hosts_booted_target $OPERATING_DRIVE "$REBOOT_HOSTS" || {
   echo "Not all hosts booted to $OPERATING_DRIVE OS, check the environment!"
@@ -59,4 +61,4 @@ backup_control_backup_all $BACKUPLINK $DRIVESET
 #}
 
 # WHY?
-# admin_control_fix_grub_these_hosts "$ALL_HOSTS"
+# admin_control_fix_grub_these_hosts "$HOSTS"
--- macros/09_reset_everything.sh
+++ macros/09_reset_everything.sh
@@ -7,21 +7,21 @@ MACRO_DIR=$( dirname $MACRO_SOURCE )
 BACKUPLINK=$1 DRIVESET=$2 HOSTS=$3
 [[ $BACKUPLINK == "" ]] && BACKUPLINK=01_CentOS_8_3_Admin_Install
 [[ $DRIVESET == "" ]] && DRIVESET=a
-[[ $HOSTNS == "" ]] && HOSTS="$ALL_HOSTS"
+[[ $HOSTS == "" ]] && HOSTS="$ALL_HOSTS"
 
 # This script will restore all hard drive OS's
 #   by default from Updated Centos8 Admin images to DriveA
 # Run from admin box (yoda)
 
 # root users also need to know all hosts, for restor rsyncs
-#for HOST in $ALL_HOSTS; do
-#  ssh_control_run_as_user root ". ~cliff/CODE/feralcoder/host_control/control_scripts.sh; ssh_control_refetch_hostkey_these_hosts \"$ALL_HOSTS\"" $HOST
+#for HOST in $HOSTS; do
+#  ssh_control_run_as_user root ". ~cliff/CODE/feralcoder/host_control/control_scripts.sh; ssh_control_refetch_hostkey_these_hosts \"$HOSTS\"" $HOST
 #done
 #
-#ssh_control_run_as_user_these_hosts root "[[ -f ~/.ssh/id_rsa ]] || ssh-keygen -t rsa -P \"\" -f ~/.ssh/id_rsa" "$ALL_HOSTS"
-#ssh_control_run_as_user_these_hosts root "cat ~/.ssh/id_rsa.pub" "$ALL_HOSTS" | grep '^ssh' > /tmp/root_pubs
-#ssh_control_sync_as_user_these_hosts root /tmp/root_pubs /tmp/root_pubs "$ALL_HOSTS"
-#ssh_control_run_as_user_these_hosts root "cat ~/.ssh/authorized_keys /tmp/root_pubs | sort | uniq > /tmp/root_pubs_$$ ; cat /tmp/root_pubs_$$ > ~/.ssh/authorized_keys" "$ALL_HOSTS"
+#ssh_control_run_as_user_these_hosts root "[[ -f ~/.ssh/id_rsa ]] || ssh-keygen -t rsa -P \"\" -f ~/.ssh/id_rsa" "$HOSTS"
+#ssh_control_run_as_user_these_hosts root "cat ~/.ssh/id_rsa.pub" "$HOSTS" | grep '^ssh' > /tmp/root_pubs
+#ssh_control_sync_as_user_these_hosts root /tmp/root_pubs /tmp/root_pubs "$HOSTS"
+#ssh_control_run_as_user_these_hosts root "cat ~/.ssh/authorized_keys /tmp/root_pubs | sort | uniq > /tmp/root_pubs_$$ ; cat /tmp/root_pubs_$$ > ~/.ssh/authorized_keys" "$HOSTS"
 
 if [[ $DRIVESET == x ]]; then
   OPERATING_DRIVE=default
@@ -33,15 +33,16 @@ fi
 
 SELFNAME_SHORT=`hostname | awk -F'.' '{print $1}'`
 NAME_SUFFIX=`echo $SELFNAME_SHORT | awk -F'-' '{print $2}'`
+REBOOT_HOSTS=`group_logic_remove_self "$HOSTS"`
 [[ $NAME_SUFFIX == "" ]] && NAME_SUFFIX=default
-[[ $NAME_SUFFIX == "$OPERATING_DRIVE" ]] || {
+# if SELF NOT OPERATING DRIVE and SELF IN DO-LIST then BAIL
+( [[ $NAME_SUFFIX != "$OPERATING_DRIVE" ]] && [[ $REBOOT_HOSTS != $HOSTS ]] ) && {
   echo "RUN THIS SCRIPT FROM AN $OPERATING_DRIVE OS!"
   echo "This host will back itself up without reboots."
   exit 1
 }
 
 
-REBOOT_HOSTS=`group_logic_remove_self "$ALL_HOSTS"`
 os_control_boot_to_target_installation_these_hosts $OPERATING_DRIVE "$REBOOT_HOSTS"
 os_control_assert_hosts_booted_target $OPERATING_DRIVE "$REBOOT_HOSTS" || {
   echo "Not all hosts booted to $OPERATING_DRIVE OS, check the environment!"
@@ -49,7 +50,7 @@ os_control_assert_hosts_booted_target $OPERATING_DRIVE "$REBOOT_HOSTS" || {
 }
 
 backup_control_restore_all $BACKUPLINK "" $DRIVESET
-admin_control_fix_grub_these_hosts "$ALL_HOSTS"
+admin_control_fix_grub_these_hosts "$HOSTS"
 
 echo; echo "GRUB MAY BE BROKEN ON TARGET DRIVE - if reboot fails, then access from $OPERATING_DRIVE GRUB, then fix grub."
 os_control_boot_to_target_installation_these_hosts $TARGET_DRIVE "$STACK_HOSTS"
@@ -57,4 +58,4 @@ os_control_assert_hosts_booted_target $TARGET_DRIVE "$REBOOT_HOSTS" || {
   echo "Not all hosts booted to $TARGET_DRIVE OS, check the environment!"
   exit 1
 }
-admin_control_fix_grub_these_hosts "$ALL_HOSTS"
+admin_control_fix_grub_these_hosts "$HOSTS"
