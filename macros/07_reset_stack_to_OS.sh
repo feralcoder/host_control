fbabbb6b8976ce179f9dfa1655e9568186d7c4cb
feralcoder <feralcoder@gmail.com>
2021-03-05 18:50:17 +0000

    Use updated OS backup.

--- macros/07_reset_stack_to_OS.sh
+++ macros/07_reset_stack_to_OS.sh
@@ -4,7 +4,7 @@ MACRO_DIR=$( dirname $MACRO_SOURCE )
 
 . ~/CODE/feralcoder/host_control/control_scripts.sh
 
-$MACRO_DIR/09_reset_everything.sh 01_CentOS_8_3_Admin_Install a "$STACK_HOSTS"
+$MACRO_DIR/09_reset_everything.sh 01b_CentOS_8_3_Admin_Install a "$STACK_HOSTS"
 
 
 
