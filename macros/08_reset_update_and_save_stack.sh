b20194dfee11db88200eb2778cf58c5c9fa37869
feralcoder <feralcoder@gmail.com>
2021-03-14 20:34:32 +0000

    requires host list

--- macros/08_reset_update_and_save_stack.sh
+++ macros/08_reset_update_and_save_stack.sh
@@ -5,5 +5,5 @@ MACRO_DIR=$( dirname $MACRO_SOURCE )
 . ~/CODE/feralcoder/host_control/control_scripts.sh
 
 $MACRO_DIR/07_reset_stack_to_OS.sh
-$MACRO_DIR/02a_hosts_update.sh
+$MACRO_DIR/02a_hosts_update.sh "$STACK_HOSTS"
 $MACRO_DIR/07_backup_stack_to_OS.sh
