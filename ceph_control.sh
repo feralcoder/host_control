90c4ac7e6ca03bec2649111780a6e7f642298ecc
feralcoder <feralcoder@gmail.com>
2021-03-15 03:54:31 +0000

    ceph_contorl_map_OSDs to track OSD UUIDs

--- ceph_control.sh
+++ ceph_control.sh
@@ -26,3 +26,43 @@ ceph_control_setup_OSD () {
   }
 }
 
+
+
+# WARNING: /dev/xxx CAN CHANGE ACROSS REBOOT!!!
+#  verify by UUID
+
+# for HOST in $OSD_HOSTS ; do ceph_control_map_OSDs $HOST; done
+# mtn:OSD_1_db:/dev/sde:5DF26BAA-0185-4F57-8763-DB5378711819
+# mtn:OSD_1_data:/dev/sdd:2ABBF474-5FAC-482A-9F2E-C6061790B505
+# lmn:OSD_1_db:/dev/sde:1BDD2355-3776-48FF-A704-87C49BD8333E
+# lmn:OSD_1_data:/dev/sdd:676E27B6-057F-460D-B6A8-960B7E590A03
+# bmn:OSD_1_db:/dev/sde:632DFF71-2758-4DB2-8092-D525BA42A58C
+# bmn:OSD_1_data:/dev/sdd:8877C770-20BD-4826-BB74-511AF41D7AD2
+# neo:OSD_1_db:/dev/sde:A784A191-2CEC-49AF-B8A9-6F590F615299
+# neo:OSD_1_data:/dev/sdc:9CBA2B15-FB1E-4AF8-8BFF-8AE8558F27BA
+# kgn:OSD_1_db:/dev/sdg:F0C24FA2-BFE0-4445-AE10-05D68E31A13B
+# kgn:OSD_1_data:/dev/sdf:6DFC0E9F-2411-4084-A069-968E766E6F6F
+
+
+ceph_control_map_OSDs () {
+  local HOST=$1
+  local OSDS VGS PV_VG_MAP
+
+  OSD_VOLS=`ssh_control_run_as_user root "lvdisplay" $HOST | grep OSD_ | grep 'LV Path'`
+  VGS=`echo "$OSD_VOLS" | awk '{print $3}' | awk -F'/' '{print $3}'`
+  PV_VG_MAP=`ssh_control_run_as_user root "pvdisplay -C --separator '  |  ' -o pv_name,vg_name" $HOST | grep '/dev/'`
+
+  local DEVICE
+  PV_UUID_MAP=$(for PV in $(echo "$PV_VG_MAP" | awk '{print $1}'); do 
+    DEVICE=$( echo $PV | sed 's/[0-9]$//g' )
+    UUID=$( ssh_control_run_as_user root "fdisk -l $DEVICE" $HOST | grep "Disk identifier:" | awk '{print $3}')
+    echo $PV $UUID
+  done)
+
+  for VG in $VGS; do
+    PV=$(echo "$PV_VG_MAP" | grep $VG | awk '{print $1}')
+    DEVICE=$(echo $PV | sed 's/[0-9]$//g')
+    UUID=$(echo "$PV_UUID_MAP" | grep $DEVICE | awk '{print $2}')
+    echo $HOST:$VG:$DEVICE:$UUID
+  done
+}
