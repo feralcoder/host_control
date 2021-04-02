651ccf5663ccbc70c3f05bae8b4ae1acdc67de24
feralcoder <feralcoder@gmail.com>
2021-04-02 20:15:48 +0000

    add some safeties so password prompts don't time out, and so user knows to toggle grub selections.

--- macros/00c_new_install_with_existing_admin_key.sh
+++ macros/00c_new_install_with_existing_admin_key.sh
@@ -22,50 +22,73 @@ HOST=$1 DEVICE=$2 PREFIX=$3
 SHORT_NAME=`group_logic_get_short_name $HOST`
 
 
-echo; echo "FIXING KEYS ON $HOST"
-ilo_control_refetch_ilo_hostkey_these_hosts $HOST
-ssh_control_refetch_hostkey_these_hosts $HOST
-admin_control_sync_keys_from_admin $HOST
-ssh_control_refetch_hostkey_these_hosts $HOST
 
-echo; echo "FIXING HD GRUB (post-install, pre-label) ON $HOST"
-admin_control_make_no_crossboot $HOST
-admin_control_fix_grub_os_prober $HOST
-admin_control_fix_grub $HOST
+fix_keys () {
+  echo; echo "FIXING KEYS ON $HOST"
+  ilo_control_refetch_ilo_hostkey_these_hosts $HOST
+  ssh_control_refetch_hostkey_these_hosts $HOST
 
-[[ $DEVICE != "" ]] || DEVICE=`ssh_control_run_as_user root "mount" $HOST | grep ' / ' | awk '{print $1}' | sed 's/[0-9]*//g'`
-[[ $PREFIX != "" ]] || {
-  BOOT_LINE=`ssh_control_run_as_user root "blkid" $HOST | grep $DEVICE | grep 'LABEL=".*_boot"'`
-  PREFIX=`echo $BOOT_LINE | sed 's/.*LABEL="//g' | sed "s/${SHORT_NAME}_boot.*//g"`
+  echo; echo "SYNCING AUTH FROM ADMIN INSTALLATION"
+  echo "The next step may prompt for password, so I will wait until you type 'yes'."
+  CONTINUE="wait"
+  while [[ ${CONTINUE,,} != "yes" ]]; do
+    read -p "Type 'yes' here: " CONTINUE
+  done
+  admin_control_sync_keys_from_admin $HOST
+
+  ssh_control_refetch_hostkey_these_hosts $HOST
 }
 
 
-echo; echo "BOOTING $HOST TO ADMIN TO RELABEL"
-os_control_boot_to_target_installation admin $HOST
-os_control_assert_hosts_booted_target admin $HOST || { echo "Failed to boot to admin!"; exit 1; }
-DEVICE=`ssh_control_run_as_user root "blkid" $HOST | grep ${PREFIX}${SHORT_NAME}_boot | awk '{print $1}' | sed 's/[0-9]:.*//g'`
-admin_control_fix_labels $DEVICE $PREFIX $HOST
-admin_control_fix_grub $HOST -1
-ilo_power_off $HOST
-ilo_power_wait_for_off $HOST
+
+fix_boots () {
+  # We should be booted to DEFAULT right now...
+  echo; echo "FIXING HD GRUB (post-install, pre-label) ON $HOST HD INSTALL"
+  admin_control_fix_grub $HOST
+
+  [[ $DEVICE != "" ]] || DEVICE=`ssh_control_run_as_user root "mount" $HOST | grep ' / ' | awk '{print $1}' | sed 's/[0-9]*//g'`
+  [[ $PREFIX != "" ]] || {
+    BOOT_LINE=`ssh_control_run_as_user root "blkid" $HOST | grep $DEVICE | grep 'LABEL=".*_boot"'`
+    PREFIX=`echo $BOOT_LINE | sed 's/.*LABEL="//g' | sed "s/${SHORT_NAME}_boot.*//g"`
+  }
+
+  echo; echo "BOOTING $HOST TO ADMIN TO RELABEL"
+  os_control_boot_to_target_installation admin $HOST
+  os_control_assert_hosts_booted_target admin $HOST || { echo "Failed to boot to admin!"; exit 1; }
+  DEVICE=`ssh_control_run_as_user root "blkid" $HOST | grep ${PREFIX}${SHORT_NAME}_boot | awk '{print $1}' | sed 's/[0-9]:.*//g'`
+  admin_control_fix_labels $DEVICE $PREFIX $HOST
+  admin_control_fix_grub $HOST -1
+  ilo_power_off $HOST
+  ilo_power_wait_for_off $HOST
+
+  echo; echo "BOOTING $HOST TO DEFAULT TO FIX GRUB"
+  echo "The next step will boot to USB, hang, and you must select HD target.  Understand?"
+  CONTINUE="wait"
+  while [[ ${CONTINUE,,} != "yes" ]]; do
+    read -p "Type 'yes' here: " CONTINUE
+  done
+  ilo_boot_target_once $DEV_USB $HOST
+  ssh_control_wait_for_host_up $HOST
+  os_control_assert_hosts_booted_target default $HOST || { echo "Failed to boot to default for grub fix!"; exit 1; }
+
+  admin_control_fix_grub $HOST
+  ilo_power_off $HOST
+
+  echo; echo "BOOTING $HOST TO ADMIN TO UNSET INFINITE GRUB"
+  os_control_boot_to_target_installation admin $HOST
+  os_control_assert_hosts_booted_target admin $HOST || { echo "Failed to boot to admin for grub fix!"; exit 1; }
+  admin_control_fix_grub $HOST
+
+  echo; echo "REBOOTING $HOST TO DEFAULT"
+  os_control_boot_to_target_installation default $HOST
+  os_control_assert_hosts_booted_target default $HOST || { echo "Failed to boot to default!"; exit 1; }
+}
 
 
-echo; echo "BOOTING $HOST TO DEFAULT TO FIX GRUB"
-ilo_boot_target_once $DEV_USB $HOST
-ssh_control_wait_for_host_up $HOST
-os_control_assert_hosts_booted_target default $HOST || { echo "Failed to boot to default for grub fix!"; exit 1; }
-
-admin_control_fix_grub $HOST
-ilo_power_off $HOST
 
 
 
-echo; echo "BOOTING $HOST TO ADMIN TO UNSET INFINITE GRUB"
-os_control_boot_to_target_installation admin $HOST
-os_control_assert_hosts_booted_target admin $HOST || { echo "Failed to boot to admin for grub fix!"; exit 1; }
-admin_control_fix_grub $HOST
+fix_keys
+fix_boots
 
 
-echo; echo "REBOOTING $HOST TO DEFAULT"
-os_control_boot_to_target_installation default $HOST
-os_control_assert_hosts_booted_target default $HOST || { echo "Failed to boot to default!"; exit 1; }
