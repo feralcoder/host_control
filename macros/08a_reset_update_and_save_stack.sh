621858e5bae2c5f20d14a285435794fabf732b8b
feralcoder <feralcoder@gmail.com>
2021-04-08 15:32:08 +0000

    split remediate_access from remediate, for better control

--- macros/08a_reset_update_and_save_stack.sh
+++ macros/08a_reset_update_and_save_stack.sh
@@ -11,32 +11,49 @@ fi
 
 . ~/CODE/feralcoder/host_control/control_scripts.sh
 
-RESTORE_FROM=01_CentOS_8_3_Admin_Install_REBUILT
-#RESTORE_FROM=01b_CentOS_8_3_Admin_Install_REBUILT
+#RESTORE_FROM=01_CentOS_8_3_Admin_Install_REBUILT
+RESTORE_FROM=01b_CentOS_8_3_Admin_Install_REBUILT
 #RESTORE_FROM=01c_CentOS_8_3_Remediated
 #RESTORE_FROM=01d_CentOS_8_3_Postmediated
 #RESTORE_FROM=02a_Kolla-Ansible_Setup
 #RESTORE_FROM=02b_Ceph_Setup
 
 #BACKUP_TO=01_CentOS_8_3_Admin_Install_REBUILT
-BACKUP_TO=01b_CentOS_8_3_Admin_Install_REBUILT
-#BACKUP_TO=01c_CentOS_8_3_Remediated
+#BACKUP_TO=01b_CentOS_8_3_Admin_Install_REBUILT
+BACKUP_TO=01c_CentOS_8_3_Remediated
 #BACKUP_TO=01d_CentOS_8_3_Postmediated
 #BACKUP_TO=02a_Kolla-Ansible_Setup
 #BACKUP_TO=02b_Ceph_Setup
 
 
-remediate () {
+
+
+
+remediate_access () {
 # root users also need to know all hosts, for backup rsyncs
   for HOST in $STACK_HOSTS; do
     ssh_control_run_as_user root ". ~cliff/CODE/feralcoder/host_control/control_scripts.sh; ssh_control_refetch_hostkey_these_hosts \"$HOSTS\"" $HOST                  || return 1
   done
-  
+
   ROOT_PUBS=/tmp/root_pubs_$$
   ssh_control_run_as_user_these_hosts root "[[ -f ~/.ssh/id_rsa ]] || ssh-keygen -t rsa -P \"\" -f ~/.ssh/id_rsa" "$ALL_HOSTS"                                         || return 1
   ssh_control_run_as_user_these_hosts root "cat ~/.ssh/id_rsa.pub" "$STACK_HOSTS" | grep '^ssh' > $ROOT_PUBS                                                           || return 1
   ssh_control_sync_as_user_these_hosts root $ROOT_PUBS $ROOT_PUBS "$STACK_HOSTS"                                                                                       || return 1
   ssh_control_run_as_user_these_hosts root "cat ~/.ssh/authorized_keys $ROOT_PUBS | sort | uniq > ${ROOT_PUBS}x ; cat ${ROOT_PUBS}x > ~/.ssh/authorized_keys" "$HOSTS" || return 1
+}
+
+remediate () {
+  # Admin and Pager
+  ssh_control_run_as_user_these_hosts cliff "cd ~/CODE/feralcoder/workstation && git pull" "$ALL_HOSTS"
+  ssh_control_run_as_user_these_hosts cliff "~/CODE/feralcoder/workstation/update.sh" "$ALL_HOSTS" || return 1
+  ssh_control_run_as_user_these_hosts cliff "python3 $TWILIO_PAGER_DIR/pager.py \"hello from \`hostname\`\"" "$STACK_HOSTS"
+  
+  # Performance Tools
+  ssh_control_run_as_user_these_hosts cliff "( grep 'PATH.*share.bcc.tools' .bash_profile ) && sed -i 's|.*PATH.*share.bcc.tools.*|export PATH=\$PATH:/usr/share/bcc/tools:~cliff/CODE/brendangregg/perf-tools|g' .bash_profile || echo 'export PATH=\$PATH:/usr/share/bcc/tools:~cliff/CODE/brendangregg/perf-tools' >> ~/.bash_profile" "$STACK_HOSTS"
+  ssh_control_run_as_user_these_hosts root "( grep 'PATH.*share.bcc.tools' .bash_profile ) && sed -i 's|.*PATH.*share.bcc.tools.*|export PATH=\$PATH:/usr/share/bcc/tools:~cliff/CODE/brendangregg/perf-tools|g' .bash_profile || echo 'export PATH=\$PATH:/usr/share/bcc/tools:~cliff/CODE/brendangregg/perf-tools' >> ~/.bash_profile" "$STACK_HOSTS"
+
+  # Setup Dumbledore as Stack Controller
+  ssh_control_run_as_user cliff "rm .local_settings && cd ~/CODE/feralcoder/bootstrap-scripts && ./stack_control.sh" $ANSIBLE_CONTROLLER
 }
 
 fail_exit () {
@@ -68,6 +85,7 @@ echo; echo "BOOTING TO DEFAULT OS ON $STACK_HOSTS"
 boot_to_target default                                            || fail_exit "boot_to_target default"
 echo; echo "ALL HOSTS ARE BOOTED TO default"
 
+remediate_access                                                  || fail_exit "remediate_access"
 remediate                                                         || fail_exit "remediate"
 
 $MACRO_DIR/02a_hosts_update.sh "$STACK_HOSTS"                     || fail_exit "02a_hosts_update.sh \"$STACK_HOSTS\""
